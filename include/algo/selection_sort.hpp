4be212c6ab251638cdd0ab27d80b3e8d93a8b556
Marek Kurdej <mkurdej@users.noreply.github.com>
2016-09-20 15:27:37 +0200

    Update selection_sort.

--- include/algo/selection_sort.hpp
+++ include/algo/selection_sort.hpp
@@ -3,11 +3,19 @@
 
 namespace mk {
 
-template <class ForwardIt, class Compare = std::less<typename std::iterator_traits<ForwardIt>::value_type>>
-void selection_sort(ForwardIt begin, ForwardIt end, Compare comp = {}) {
+template <class ForwardIt, class Compare>
+void selection_sort(ForwardIt begin, ForwardIt end, Compare comp)
+{
     for (auto i = begin; i != end; ++i) {
         std::iter_swap(i, std::min_element(i, end, comp));
     }
+}
+
+template <class ForwardIt>
+void selection_sort(ForwardIt begin, ForwardIt end)
+{
+    using value_type = typename std::iterator_traits<ForwardIt>::value_type;
+    selection_sort(begin, end, std::less<value_type>());
 }
 
 } // namespace mk
