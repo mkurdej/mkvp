2d9de861727dd564ae1cf2cbf4e7d1aeaf0d8256
Marek Kurdej <mkurdej@users.noreply.github.com>
2016-09-20 15:38:07 +0200

    Use C++14 style.

--- include/algo/selection_sort.hpp
+++ include/algo/selection_sort.hpp
@@ -1,21 +1,13 @@
 #include <algorithm>
-#include <iterator>
 
 namespace mk {
 
-template <class ForwardIt, class Compare>
-void selection_sort(ForwardIt begin, ForwardIt end, Compare comp)
+template <class ForwardIt, class Compare = std::less<>>
+void selection_sort(ForwardIt begin, ForwardIt end, Compare comp = {})
 {
     for (auto i = begin; i != end; ++i) {
         std::iter_swap(i, std::min_element(i, end, comp));
     }
-}
-
-template <class ForwardIt>
-void selection_sort(ForwardIt begin, ForwardIt end)
-{
-    using value_type = typename std::iterator_traits<ForwardIt>::value_type;
-    selection_sort(begin, end, std::less<value_type>());
 }
 
 } // namespace mk
