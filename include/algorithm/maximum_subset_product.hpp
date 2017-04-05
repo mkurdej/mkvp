05d80806a7ff4ffb0d6e90b431b35ae9a53281fe
Marek Kurdej <marek@quasardb.net>
2017-04-05 16:02:37 +0200

    First refactor.

--- include/algorithm/maximum_subset_product.hpp
+++ include/algorithm/maximum_subset_product.hpp
@@ -1,70 +1,80 @@
-#include <limits.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <algorithm>
+#include <limits>
+#include <tuple>
 
 namespace mk {
 
-int min(int x, int y) {
-  return (x < y) ? x : y;
+namespace detail {
+
+template <typename IntegralType>
+bool is_odd(IntegralType v) {
+  return v & 1;
 }
 
-// Function to return maximum product of a subset of given array
-int maximum_subset_product(int arr[], int n) {
-  // base case
-  if (n == 0) return 0;
+} // namespace detail
 
-  // if array contains only one element
-  if (n == 1) return arr[0];
+// Function to return maximum product of a subset of given array
+template <typename Iterator, typename ValueType = typename std::iterator_traits<
+                                 Iterator>::value_type>
+std::tuple<ValueType, Iterator, Iterator>
+maximum_subset_product(Iterator first, Iterator last) {
+  const auto n = std::distance(first, last);
+
+  if (n == 0) return std::make_tuple(ValueType{}, first, last);
+  if (n == 1) return std::make_tuple(*first, first, last);
 
   int product = 1; // to store maximum product subset
 
   // stores the negative element having minimum absolute value in the set
-  int abs_min_so_far = INT_MAX;
+  int abs_min_so_far = std::numeric_limits<int>::max();
 
   int negative = 0; // maintain count of negative elements in the set
   int positive = 0; // maintain count of positive elements in the set
 
-  int contains_zero = 0;
+  bool contains_zero = false;
+
+  Iterator start = first, end = last;
 
   // traverse the given array
-  for (int i = 0; i < n; i++) {
+  for (Iterator it = first; it != last; ++it) {
     // if current element is negative
-    if (arr[i] < 0) {
-      negative++;
-      abs_min_so_far = min(abs_min_so_far, abs(arr[i]));
+    if (*it < 0) {
+      ++negative;
+      abs_min_so_far = std::min(abs_min_so_far, std::abs(*it));
     }
     // if current element is positive
-    else if (arr[i] > 0) {
-      positive++;
+    else if (*it > 0) {
+      ++positive;
     }
 
     // if current element is zero
-    if (arr[i] == 0)
-      contains_zero = 1;
-    else
-      product = product * arr[i];
+    if (*it == 0) {
+      contains_zero = true;
+    } else {
+      product = product * (*it);
+    }
   }
 
   // if odd number of negative elements are present, exclude the negative
   // element having minimum absolute value from the subset
-  if (negative & 1) product = product / -abs_min_so_far;
+  if (detail::is_odd(negative)) product = product / -abs_min_so_far;
 
   // special case - set contains one negative element and one or more zeroes
   if (negative == 1 && !positive && contains_zero) product = 0;
 
   // return maximum product
-  return product;
+  return std::make_tuple(product, start, end);
 }
 
 } // namespace mk
 
+#include <cstdio>
+
 int main() {
   int arr[] = {-6, 4, -5, 8, -10, 0, 8};
 
-  int n = sizeof(arr) / sizeof(arr[0]);
-
-  printf("The maximum product of a subset is %d",
-         maximum_subset_product(arr, n));
+  auto res = mk::maximum_subset_product(std::begin(arr), std::end(arr));
+  printf("The maximum product of a subset is %d", std::get<0>(res));
 
   return 0;
 }
